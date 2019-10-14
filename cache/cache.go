e4bb8f85956dc5bbd48e6fc85fb8357a8021f645
xin.cao <xin.cao@asiainnovations.com>
2019-10-15 01:06:44 +0800

    ok

--- cache/cache.go
+++ cache/cache.go
@@ -13,30 +13,30 @@ var (
 const open bool = true
 
 func init() {
-	C = fastcache.New (1 << 30)
+	C = fastcache.New(1 << 30)
 }
 
-func Get (key []byte) []byte{
+func Get(key []byte) []byte {
 	if open == false {
 		return nil
 	}
 	return C.Get(nil, key)
 }
 
-func Set (key []byte, val []byte) {
+func Set(key []byte, val []byte) {
 	if open == false {
 		return
 	}
 	C.Set(key, val)
 }
 
-func Del (key []byte) {
+func Del(key []byte) {
 	if open == false {
 		return
 	}
 	C.Del(key)
 }
 
-func Close () {
+func Close() {
 	C.SaveToFile(filepath.Join(appendfile.M.Dir, "cache"))
 }
