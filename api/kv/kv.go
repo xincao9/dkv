70602e0b84c5e54404c611b5f63451a0fdbd5866
xincao9 <xincao9@gmail.com>
2020-06-29 17:40:40 +0800

    对输出code进行标准化

--- api/kv/kv.go
+++ api/kv/kv.go
@@ -7,8 +7,9 @@ import (
 	"dkv/component/logger"
 	"dkv/component/metrics"
 	"dkv/store"
-	"github.com/gin-gonic/gin"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 type KV struct {
@@ -16,18 +17,25 @@ type KV struct {
 	V string `json:"v"`
 }
 
+const (
+	InvalidArgument = "invalid argument"
+	Ok              = "ok"
+	InternalError   = "internal error"
+	KeyNotFound     = "key not found"
+)
+
 func Route(engine *gin.Engine) {
 	engine.GET("/kv/:key", func(c *gin.Context) {
 		key := c.Param("key")
 		if key == "" {
-			response(c, http.StatusBadRequest, "参数错误")
+			response(c, http.StatusBadRequest, InvalidArgument)
 			metrics.GetCount.WithLabelValues("failure", "bad_request").Inc()
 			return
 		}
 		val := cache.C.Get([]byte(key))
 		if val != nil {
 			val = compress.C.Decode(val)
-			responseKV(c, http.StatusOK, "成功", &KV{K: key, V: string(val)})
+			responseKV(c, http.StatusOK, Ok, &KV{K: key, V: string(val)})
 			metrics.GetCount.WithLabelValues("success", "memory").Inc()
 			return
 		}
@@ -35,23 +43,23 @@ func Route(engine *gin.Engine) {
 		if err == nil {
 			cache.C.Set([]byte(key), val)
 			val = compress.C.Decode(val)
-			responseKV(c, http.StatusOK, "成功", &KV{K: key, V: string(val)})
+			responseKV(c, http.StatusOK, Ok, &KV{K: key, V: string(val)})
 			metrics.GetCount.WithLabelValues("success", "disk").Inc()
 			return
 		}
 		if err == constant.KeyNotFound {
-			responseKV(c, http.StatusOK, "没有找到", &KV{K: key, V: ""})
+			responseKV(c, http.StatusNotFound, KeyNotFound, &KV{K: key, V: ""})
 			metrics.GetCount.WithLabelValues("failure", "not_found").Inc()
 			return
 		}
 		logger.L.Errorf("method:get path:/kv/%s err=%s\n", key, err)
-		response(c, http.StatusInternalServerError, "服务端错误")
+		response(c, http.StatusInternalServerError, InternalError)
 		metrics.GetCount.WithLabelValues("failure", "server_error").Inc()
 	})
 	engine.PUT("/kv", func(c *gin.Context) {
 		var kv KV
 		if err := c.ShouldBindJSON(&kv); err != nil {
-			response(c, http.StatusBadRequest, "参数错误")
+			response(c, http.StatusBadRequest, InvalidArgument)
 			metrics.PutCount.WithLabelValues("failure").Inc()
 			return
 		}
@@ -59,32 +67,32 @@ func Route(engine *gin.Engine) {
 		err := store.S.Put([]byte(kv.K), val)
 		if err != nil {
 			logger.L.Errorf("method:post path:/kv/ body:%v err=%s\n", kv, err)
-			response(c, http.StatusInternalServerError, "服务端错误")
+			response(c, http.StatusInternalServerError, InternalError)
 			metrics.PutCount.WithLabelValues("failure").Inc()
 			return
 		}
 		cache.C.Del([]byte(kv.K))
-		response(c, http.StatusOK, "成功")
+		response(c, http.StatusOK, Ok)
 		metrics.PutCount.WithLabelValues("success").Inc()
 	})
 	engine.DELETE("/kv/:key", func(c *gin.Context) {
 		key := c.Param("key")
 		if key == "" {
-			response(c, http.StatusBadRequest, "参数错误")
+			response(c, http.StatusBadRequest, InvalidArgument)
 			return
 		}
 		err := store.S.Delete([]byte(key))
 		if err == nil {
 			cache.C.Del([]byte(key))
-			response(c, http.StatusOK, "成功")
+			response(c, http.StatusOK, Ok)
 			return
 		}
 		if err == constant.KeyNotFound {
-			response(c, http.StatusOK, "没有找到")
+			response(c, http.StatusNotFound, KeyNotFound)
 			return
 		}
 		logger.L.Errorf("method:delete path:/kv/%s err=%s\n", key, err)
-		response(c, http.StatusInternalServerError, "服务端错误")
+		response(c, http.StatusInternalServerError, InternalError)
 	})
 }
 
